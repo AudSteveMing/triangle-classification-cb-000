b5074b422f3b6fb42f87f832c18caf67a96a6f1d
Steve Cordova <cordovas@bc.edu>
2020-08-16 02:34:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if side_a == side_b == side_c
+    if side_a == side_b && side_b == side_c
       return :equilateral
     elsif side_a == side_b || side_b == side_c || side_a == side_c
       return :isosceles
